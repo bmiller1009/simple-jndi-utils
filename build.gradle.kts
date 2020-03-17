149ad939b5e65325ea41730ec7c570105964cc1f
Brad Miller <bfm@bradfordmiller.org>
2020-03-16 22:20:18 -0400

    Updated dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -71,7 +71,7 @@ dependencies {
     implementation("com.github.h-thurow", "simple-jndi","0.18.1")
     implementation("org.apache.logging.log4j", "log4j-slf4j-impl", "2.12.0")
     implementation("commons-io", "commons-io", "2.6")
-    implementation("io.vavr:vavr:0.9.0")
+    api("io.vavr:vavr:0.9.0")
     testImplementation("org.junit.jupiter:junit-jupiter-engine:5.5.1")
 }
 
