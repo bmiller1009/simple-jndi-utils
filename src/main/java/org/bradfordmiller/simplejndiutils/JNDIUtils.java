32a60a9af7de38ab1e7f6cffa4ec53de9bbda9e1
Brad Miller <bfm@bradfordmiller.org>
2021-11-16 14:27:09 -0500

    Removed rmi exception import

--- src/main/java/org/bradfordmiller/simplejndiutils/JNDIUtils.java
+++ src/main/java/org/bradfordmiller/simplejndiutils/JNDIUtils.java
@@ -10,7 +10,6 @@ import java.lang.reflect.Field;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.rmi.activation.UnknownObjectException;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.*;
